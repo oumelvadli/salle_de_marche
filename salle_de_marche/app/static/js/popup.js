12df1becaba2652067fa0028e0310f4181ce17d5
M1 <22069@supnum.mr>
2024-03-08 23:55:35 +0000

    sdfsd

--- salle_de_marche/app/static/js/popup.js
+++ salle_de_marche/app/static/js/popup.js
@@ -16,37 +16,17 @@ $(document).ready(function(){
         $("#addBande").modal('hide');
     });
 
-    // validation
-    $("#addCoursForm").submit(function(event) {
-      var cours = $("#coursValue").val();
-
-      if (isNaN(cours)) {
-        $("#coursError").text("Le cours doivent être des entiers.");
-        event.preventDefault();
-      } else {
-        $("#coursError").text("");
-      }
-    });
-
     $("#addBandeForm").submit(function(event) {
         // Validation du formulaire
         var max = $("#bandeMax").val();
         var min = $("#bandeMin").val();
 
 
-        if (parseInt(min) >= parseInt(max)) {
+        if (min >= max) {
             $("#minError").text("La valeur minimale doit être inférieure à la valeur maximale.");
             event.preventDefault();
         } else {
             $("#minError").text("");
         }
-
-        if (isNaN(max) || isNaN(min)) {
-          $("#minError").text("Les valeurs doivent être des entiers.");
-          event.preventDefault();
-        } else {
-          $("#minError").text("");
-        }
-
     });
 });
