e1fbb3c09cb6b6b2a503d674002feb9993721230
AYMAN <126827455+AymanMady@users.noreply.github.com>
2024-03-09 11:26:30 +0000

    Modification dans Market data

--- salle_de_marche/app/views.py
+++ salle_de_marche/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from .models import *
 from .forms import *
-# Create your views here.
+from django.contrib import messages
+
 def Accueil(request):
     return render(request,"Accueil.html",{'navbar':'Accueil'})
 
@@ -12,6 +13,7 @@ def add_cours(request):
     if request.method == "POST":
         form = Cours_revaluationForm(request.POST)
         form.save()
+        messages.success(request,"Le cours du jour a été ajouté avec succès")
         return redirect('MarketData')
 
     return render(request,"MarketData.html",{'navbar':'MarketData'})
@@ -20,6 +22,7 @@ def add_bande(request):
     if request.method == "POST":
         form = Bande_fluctuationForm(request.POST)
         form.save()
+        messages.success(request,"Le bande de fluctuation  a été ajouté avec succès.")
         return redirect('MarketData')
 
     return render(request,"MarketData.html",{'navbar':'MarketData'})
