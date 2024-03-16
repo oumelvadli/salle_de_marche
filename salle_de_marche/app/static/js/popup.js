71f42ee7be1defffbce5480335cb8031c2d5bc49
M1 <22069@supnum.mr>
2024-03-16 01:47:30 +0000

    deplacer les script vers le popup.js

--- salle_de_marche/app/static/js/popup.js
+++ salle_de_marche/app/static/js/popup.js
@@ -45,3 +45,74 @@ $(document).ready(function(){
 
     
 });
+setTimeout(function() {
+    $('#success-alert').alert('close');
+}, 3000);
+function editCours(date, devise, cours, id) {
+    document.getElementById("editCoursDate").value = date;
+    document.getElementById("editCoursDevise").value = devise;
+    document.getElementById("editCoursValue").value = cours;
+    document.getElementById("formcours").action = "/update_cours/"+id;
+
+
+    // Ouvrir le modal de modification
+    $('#editCours').modal('show');
+}
+ //______________ ici ce deux lignes ont pour but de verifier que la date est valide _________________
+ var dateActuelle = new Date().toISOString().split('T')[0];
+ document.getElementById("bandeDate").setAttribute("max", dateActuelle);
+ document.getElementById("coursDate").setAttribute("max", dateActuelle);
+ document.getElementById("editCoursDate").setAttribute("max", dateActuelle);
+ document.getElementById("editbandeDate").setAttribute("max", dateActuelle);
+ //______________ fin _______________________
+
+  function editBand(date, devise, Bid,Ask, id) {
+      document.getElementById("editbandeDate").value = date;
+      document.getElementById("editbandeDevise").value = devise;
+      document.getElementById("editbandeBid").value = Bid;
+      document.getElementById("editbandeAsk").value = Ask;
+      document.getElementById("formbande").action = "/update_bande/"+id;
+
+
+      // Ouvrir le modal de modification
+      $('#editBande').modal('show');
+  }
+    var dateActuelle = new Date().toISOString().slice(0,10);
+  
+    document.getElementById('coursDate').addEventListener('change', function(){
+        var selectedDate = this.value;
+  
+        if (selectedDate !== dateActuelle) {
+            document.getElementById('dateError').textContent = "Attention : La date sélectionnée n'est pas la date actuelle.";
+        } else {
+            document.getElementById('dateError').textContent = "";
+        }
+    });
+  
+    document.getElementById('bandeDate').addEventListener('change', function(){
+      var selectedDate = this.value;
+  
+      if (selectedDate !== dateActuelle) {
+          document.getElementById('dateErrorBande').textContent = "Attention : La date sélectionnée n'est pas la date actuelle.";
+      } else {
+          document.getElementById('dateErrorBande').textContent = "";
+      }
+  });
+  document.getElementById('editCoursDate').addEventListener('change', function(){
+    var selectedDate = this.value;
+
+    if (selectedDate !== dateActuelle) {
+        document.getElementById('editDateError').textContent = "Attention : La date sélectionnée n'est pas la date actuelle.";
+    } else {
+        document.getElementById('editDateError').textContent = "";
+    }
+});
+document.getElementById('editbandeDate').addEventListener('change', function(){
+  var selectedDate = this.value;
+
+  if (selectedDate !== dateActuelle) {
+      document.getElementById('editDateErrorBande').textContent = "Attention : La date sélectionnée n'est pas la date actuelle.";
+  } else {
+      document.getElementById('editDateErrorBande').textContent = "";
+  }
+});
